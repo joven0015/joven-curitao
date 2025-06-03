2683e79edea0346a5c7a79708ea03bee0d6fd092
joven0015 <jovencuritao06@gmail.com>
2025-06-03 09:43:14 +0800

    iloveyou message

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const iloveyou = "HI MAMA I LOVE YOU SO MUCH WORK WEEELL <3";
   const greetings = "Hello World";
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
+        {iloveyou}
         <p>You clicked {count} times</p>
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={() => setCount(count + 1)}>Click me</button>
