d255096cff9c6380e0d2cd13a6925caad64bccfa
joven0015 <jovencuritao06@gmail.com>
2025-07-19 14:19:11 +0800

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,34 +3,37 @@ import "./Header.css";
 import Avatar from "../../images/Avatar.png";
 // const lorem = new LoremIpsum();
 // const text = lorem.generateWords(50);
+
 function Header() {
-  const text = `A passionate developer who enjoys turning ideas into interactive, user-friendly web applications.\n 
-  
-I’m currently strengthening my skills in JavaScript, React, Node.js, and Supabase by building real-world projects from scratch.\n
-
-My goal is to grow into a full-stack developer who builds smooth, user-centered experiences.\n
+  const text = ` a curious web developer who loves turning ideas into simple, smooth web experiences.
+I'm growing my skills in React, Node, and Supabase by building and learning every day.
 This portfolio showcases my hands-on learning journey, my curiosity for technology, and my love for solving problems through code.\n
-
 I'm always eager to level up, connect with others in tech, and take on new challenges as I grow.`;
 
   return (
     <header id="header" className="header">
-      <div id="hero">
-        <div id="intro">
-          <div>
-            <div id="Hi">Hi I’m</div>
-            <div id="full_name">Joven Curitao</div>
-            <div id="career">Computer Engineer & Developer</div>
-            <div id="description">
-              <p>{text}</p>
-            </div>
+      <section className="hero">
+        <div className="hero_container">
+          <div className="intro hi">
+            <h1>Hi I’m</h1>
           </div>
+          <div className="intro name">
+            <h1>Joven Curitao</h1>
+          </div>
+          <div className="intro title">
+            <p>Chasing elegance through the chaos of creative computation.</p>
+          </div>
+          <div className="intro description">
+            I’m an aspiring software engineer focused on creating clean,
+            user-friendly digital experiences by turning complex problems into
+            simple, elegant solutions and building scalable, intuitive software.
+          </div>
+          <div className="intro button">
+            <button>button</button>
+          </div>
+          <div className="intro img">img</div>
         </div>
-        <div id="intro_img">
-          <img src={Avatar} alt="Avatar"></img>
-          <div class="ball-shadow"></div>
-        </div>
-      </div>
+      </section>
     </header>
   );
 }
